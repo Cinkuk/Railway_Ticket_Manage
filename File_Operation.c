54dc9bdd0ed67025c4f5a00621839236a1482905
Cinkuk <108922408+Cinkuk@users.noreply.github.com>
2023-12-09 13:25:06 +0800

    NAMEFUNC SaveTOHD()
    NAMEFUNC LoadFromHD()

--- File_Operation.c
+++ File_Operation.c
@@ -6,4 +6,35 @@
 //
 // 函数与功能对应：
 // 
+// // 存盘
+// Status SaveToHD()
+// input: None
+// return: OK, NOSPACE, ERROR（文件无法打开、被占用）
+// 
+// // 从磁盘读取
+// Status LoadFromHD()
+// input: None
+// return: OK, ERROR(无此文件),EMPTY(磁盘空间不足) 
+// 
+// 
+// 
+// 
+// 
+//  
 
+#include "F_Lib.h"
+#include "V_Lib.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// 存盘
+Status SaveToHD()
+{
+
+}
+
+// 从磁盘读取
+Status LoadFromHD()
+{
+
+}
